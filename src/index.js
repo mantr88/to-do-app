4e956917d2a182afd5b89c72f788359b81b16451
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-05-21 10:22:44 +0300

    added redux devTools

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { StrictMode } from 'react';
+import { Provider } from 'react-redux';
+import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
