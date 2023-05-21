937eba10e6442c755d15bd32d98b859e45d66ed2
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-05-21 12:48:45 +0300

    added selectors

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,12 +1,19 @@
+// Імпортуємо хук
+import { useSelector } from 'react-redux';
 import { Button } from 'components/Button/Button';
 import css from './StatusFilter.module.css';
+// Імпортуємо об'єкт значень фільтра
+import { statusFilters } from 'redux/constants';
+import { getStatusFilter } from 'redux/selectors';
 
 export const StatusFilter = () => {
+  // Отримуємо значення фільтра із стану Redux
+  const filter = useSelector(getStatusFilter);
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button selected={filter === statusFilters.all}>All</Button>
+      <Button selected={filter === statusFilters.active}>Active</Button>
+      <Button selected={filter === statusFilters.completed}>Completed</Button>
     </div>
   );
 };
