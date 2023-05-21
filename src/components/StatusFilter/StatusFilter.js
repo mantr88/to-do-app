3d4474eda80c84ee42142a63467de408de8d1cc3
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-05-21 13:27:55 +0300

    added actions

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,19 +1,43 @@
-// Імпортуємо хук
-import { useSelector } from 'react-redux';
-import { Button } from 'components/Button/Button';
-import css from './StatusFilter.module.css';
+// Імпортуємо хуки
+import { useSelector, useDispatch } from 'react-redux';
 // Імпортуємо об'єкт значень фільтра
 import { statusFilters } from 'redux/constants';
+// Імпортуємо генератор екшену
+import { setStatusFilter } from 'redux/actions';
+
+import { Button } from 'components/Button/Button';
+import css from './StatusFilter.module.css';
 import { getStatusFilter } from 'redux/selectors';
 
 export const StatusFilter = () => {
+  // Отримуємо посилання на функцію відправки екшенів
+  const dispatch = useDispatch();
   // Отримуємо значення фільтра із стану Redux
   const filter = useSelector(getStatusFilter);
+  // Викликаємо генератор екшену та передаємо значення фільтра
+  // Відправляємо результат - екшен зміни фільтра
+  const handleFilterChange = filter => dispatch(setStatusFilter(filter));
+
   return (
     <div className={css.wrapper}>
-      <Button selected={filter === statusFilters.all}>All</Button>
-      <Button selected={filter === statusFilters.active}>Active</Button>
-      <Button selected={filter === statusFilters.completed}>Completed</Button>
+      <Button
+        selected={filter === statusFilters.all}
+        onClick={() => handleFilterChange(statusFilters.all)}
+      >
+        All
+      </Button>
+      <Button
+        selected={filter === statusFilters.active}
+        onClick={() => handleFilterChange(statusFilters.active)}
+      >
+        Active
+      </Button>
+      <Button
+        selected={filter === statusFilters.completed}
+        onClick={() => handleFilterChange(statusFilters.completed)}
+      >
+        Completed
+      </Button>
     </div>
   );
 };
